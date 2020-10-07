f27e2884b64a5a10748fc92ead8b2897cbc55983
Irfan <qcutterz@gmail.com>
2020-10-07 12:10:00 +0700

    fix footer copyright

--- application/modules/backend/views/partials/footer.php
+++ application/modules/backend/views/partials/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
 
         <div class="copyright ml-auto">
-            2020 &copy; <a href="#">FIFGROUP</a>
+            2020 &copy; <a href="https://github.com/putradongkal/OpenSIKIL/">OpenSikil</a> theme by <a href="https://themekita.com/demo-atlantis-bootstrap/">Atlantis</a>
         </div>
     </div>
 </footer>
